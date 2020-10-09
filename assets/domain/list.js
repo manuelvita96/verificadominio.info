86d35763ae2e44ced7d565b46c9a07e19aa83577
io@bu.lc <io@bu.lc>
2020-10-09 09:31:42 +0200

    First launch
    Created homepage, url page and first list of domain

---
+++ assets/domain/list.js
@@ -0,0 +1,18 @@
+document.write('\
+\<!--<li style="display: none;"><a href="url.html?N=NOME&U=https://DOMINIO&D=09/10/2020">NOME</a></li>-->\
+\<li style="display: none;"><a href="url.html?N=Amazon&U=https://www.amazon.it&D=09/10/2020">Amazon</a></li>\
+\<li style="display: none;"><a href="url.html?N=Facebook&U=https://www.facebook.com&D=09/10/2020">Facebook</a></li>\
+\<li style="display: none;"><a href="url.html?N=Instagram&U=https://www.instagram.com&D=09/10/2020">Instagram</a></li>\
+\<li style="display: none;"><a href="url.html?N=CB01&U=https://cb01.events&D=09/10/2020">CB01</a></li>\
+\<li style="display: none;"><a href="url.html?N=EuroStreaming&U=https://eurostreaming.tel/&D=09/10/2020">EuroStreaming</a></li>\
+\<li style="display: none;"><a href="url.html?N=Netflix&U=https://netflix.com/&D=09/10/2020">Netflix</a></li>\
+\<li style="display: none;"><a href="url.html?N=CinemaGratis&U=https://cinemagratis.world&D=09/10/2020">CinemaGratis</a></li>\
+\<li style="display: none;"><a href="url.html?N=CinemaHD&U=https://cinemahd.guru&D=09/10/2020">CinemaHD</a></li>\
+\<li style="display: none;"><a href="url.html?N=FilmStreamingITA&U=https://filmstreamingita.buzz&D=09/10/2020">FilmStreamingITA</a></li>\
+\<li style="display: none;"><a href="url.html?N=CasaCinema&U=https://casacinema.work&D=09/10/2020">CasaCinema</a></li>\
+\<li style="display: none;"><a href="url.html?N=FilmStreamingIT&U=https://filmstreaming-it.life&D=09/10/2020">FilmStreamingIT</a></li>\
+\<li style="display: none;"><a href="url.html?N=FilmSenzaLimiti&U=https://filmsenzalimiti01.casa&D=09/10/2020">FilmSenzaLimiti</a></li>\
+\<li style="display: none;"><a href="url.html?N=Film4k&U=https://film4k.best&D=09/10/2020">Film4k</a></li>\
+\<li style="display: none;"><a href="url.html?N=IlGenioDelloStreaming&U=https://Ilgeniodellostreaming.rs&D=09/10/2020">IlGenioDelloStreaming</a></li>\
+\<li style="display: none;"><a href="url.html?N=GuardaSerie&U=https://guardaserie.cam&D=09/10/2020">GuardaSerie</a></li>\
+');
