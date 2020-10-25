7f233b8c3d75f98535a42afaf29fd0ed67ebcdff
io@bu.lc <io@bu.lc>
2020-10-25 07:50:50 +0100

    Update list.js
    Domain update

--- assets/domain/list.js
+++ assets/domain/list.js
@@ -3,7 +3,7 @@ document.write('\
 \<li style="display: none;"><a href="url.html?N=Amazon&U=https://www.amazon.it&D=09/10/2020">Amazon</a></li>\
 \<li style="display: none;"><a href="url.html?N=Facebook&U=https://www.facebook.com&D=09/10/2020">Facebook</a></li>\
 \<li style="display: none;"><a href="url.html?N=Instagram&U=https://www.instagram.com&D=09/10/2020">Instagram</a></li>\
-\<li style="display: none;"><a href="url.html?N=CB01&U=https://cb01.events&D=09/10/2020">CB01</a></li>\
+\<li style="display: none;"><a href="url.html?N=CB01&U=https://cb01.solutions&D=25/10/2020">CB01</a></li>\
 \<li style="display: none;"><a href="url.html?N=EuroStreaming&U=https://eurostreaming.tel/&D=09/10/2020">EuroStreaming</a></li>\
 \<li style="display: none;"><a href="url.html?N=Netflix&U=https://netflix.com/&D=09/10/2020">Netflix</a></li>\
 \<li style="display: none;"><a href="url.html?N=CinemaGratis&U=https://cinemagratis.world&D=09/10/2020">CinemaGratis</a></li>\
