939c7df03f66bd55f5b657504c63f0b2c988d332
io@bu.lc <io@bu.lc>
2021-01-12 09:56:27 +0100

    Update list.js

--- assets/domain/list.js
+++ assets/domain/list.js
@@ -3,8 +3,8 @@ document.write('\
 \<li style="display: none;"><a href="url.html?N=Amazon&U=https://www.amazon.it&D=09/10/2020">Amazon</a></li>\
 \<li style="display: none;"><a href="url.html?N=Facebook&U=https://www.facebook.com&D=09/10/2020">Facebook</a></li>\
 \<li style="display: none;"><a href="url.html?N=Instagram&U=https://www.instagram.com&D=09/10/2020">Instagram</a></li>\
-\<li style="display: none;"><a href="url.html?N=CB01&U=https://cb01.wales&D=13/11/2020">CB01</a></li>\
-\<li style="display: none;"><a href="url.html?N=EuroStreaming&U=https://eurostreaming.wiki/&D=03/12/2020">EuroStreaming</a></li>\
+\<li style="display: none;"><a href="url.html?N=CB01&U=https://cb01.directory&D=11/12/2020">CB01</a></li>\
+\<li style="display: none;"><a href="url.html?N=EuroStreaming&U=https://eurostreaming.chat/&D=12/01/2021">EuroStreaming</a></li>\
 \<li style="display: none;"><a href="url.html?N=Netflix&U=https://netflix.com/&D=09/10/2020">Netflix</a></li>\
 \<li style="display: none;"><a href="url.html?N=CinemaGratis&U=https://cinemagratis.world&D=09/10/2020">CinemaGratis</a></li>\
 \<li style="display: none;"><a href="url.html?N=CinemaHD&U=https://cinemahd.guru&D=09/10/2020">CinemaHD</a></li>\
@@ -13,6 +13,6 @@ document.write('\
 \<li style="display: none;"><a href="url.html?N=FilmStreamingIT&U=https://filmstreaming-it.life&D=09/10/2020">FilmStreamingIT</a></li>\
 \<li style="display: none;"><a href="url.html?N=FilmSenzaLimiti&U=https://filmsenzalimiti01.casa&D=09/10/2020">FilmSenzaLimiti</a></li>\
 \<li style="display: none;"><a href="url.html?N=Film4k&U=https://film4k.best&D=09/10/2020">Film4k</a></li>\
-\<li style="display: none;"><a href="url.html?N=IlGenioDelloStreaming&U=https://ilgeniodellostreaming.cat&D=20/11/2020">IlGenioDelloStreaming</a></li>\
+\<li style="display: none;"><a href="url.html?N=IlGenioDelloStreaming&U=https://ilgeniodellostreaming.cab&D=12/01/2021">IlGenioDelloStreaming</a></li>\
 \<li style="display: none;"><a href="url.html?N=GuardaSerie&U=https://guardaserie.cam&D=09/10/2020">GuardaSerie</a></li>\
 ');
